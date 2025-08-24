e635cb249fc803180b5d6b65f1824ad2fde143e2
alimoncul <alimoncul@gmail.com>
2025-08-24 13:10:08 +0300

    fix viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production'
-    ? '/takipciler.oynuyor.web/results'
+    ? '/takipciler.oynuyor.web/'
     : './',
   build: {
     outDir: 'dist',
